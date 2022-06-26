6844a7176b4f2d31ba9f9ed74b7eb80b48414634
LuisEstero <104272882+LuisEstero@users.noreply.github.com>
2022-06-26 18:43:11 +0200

    Paso 3

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,22 @@
+const { response } = require('express');
 const express = require('express');
 const router = express.Router();
+const Movie = require('./../models/Movie.model')
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+// Movies
+
+router.get('/movies' , (req, res) => {
+Movie.find()
+.then(response => {
+    console.log(response)
+    res.render("movies.hbs", { response});
+})
+.catch(error => console.log(error));
+})
+
+
 
 module.exports = router;
